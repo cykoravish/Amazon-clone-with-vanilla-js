f102b1a3036dcf2191b0f2ad7f3e035724f20749
Ravish Bisht <ravishbisht85@gmail.com>
2024-09-16 17:55:25 +0530

    ading cart quantity feature

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -58,7 +58,7 @@ products.forEach((product) => {
 document.querySelector(".js-products-grid").innerHTML = productsHTML;
 
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
-  button.addEventListener("click", (e) => {
+  button.addEventListener("click", () => {
     const productId = button.dataset.productId;
 
     let matchingItem;
@@ -76,6 +76,15 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
         quantity: 1,
       });
     }
+
+    let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+
+    console.log(cartQuantity);
     console.log(cart);
   });
 });
